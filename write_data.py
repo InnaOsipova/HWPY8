e3cc0b2704b0303d0b75fdd74fc525df1af67ac0
Inna Osipova <rutheva_inna@mail.ru>
2022-10-18 20:30:35 +0300

    Запись

--- write_data.py
+++ write_data.py
@@ -4,7 +4,13 @@ table = ['last_name', 'name', 'phone_number', 'post', "salary"]
 
 
 def get_data():
-    record = [input(f'{i} :') for i in table]
+    choise = input ('Выберите список ввода 1 - однострочный, 2 - многострочный : ')
+    if choise == '1':
+        print ('Введите фамилию, имя, номер телефона, должность, заработную плату через запятую в одну строку :')
+        record = input().split(',')
+        print(record)
+    elif choise == '2':
+        record = [input(f'{i} :') for i in table]
     with open('data.csv', 'a') as f:
         writer = csv.writer(f)
         writer.writerow(record)
